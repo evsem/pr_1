77023c53375626381616e1daae2412c42aa63f61
evsem <frukwork@gmail.com>
2022-03-02 14:29:31 +0300

    startProject

---
+++ src/UI/SelectGrey/SelectGrey.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import classes from './SelectGrey.module.css'
+
+const SelectGrey = () => {
+  return <select className={classes.selectGrey}></select>
+}
+
+export default SelectGrey
